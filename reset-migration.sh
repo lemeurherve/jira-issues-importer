106022857804ad4f1b624bbb1a9cfe02e4fa0f51
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2025-11-20 20:20:18 +0100

    chore(reset-migration): use env vars and ask for confirmation (#50)

--- reset-migration.sh
+++ reset-migration.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-REPO=$1
+: "${JIRA_MIGRATION_GITHUB_NAME:? Missing GitHub org name (e.g., jenkins-infra)}"
+: "${JIRA_MIGRATION_GITHUB_REPO:? Missing GitHub repo name (e.g., helpdesk)}"
+
+repo="${JIRA_MIGRATION_GITHUB_NAME}/${JIRA_MIGRATION_GITHUB_REPO}"
 
 # caution make sure anything you want to keep is managed in code
 # i.e. existing labels that may be used for pull requests
+read -p "Are you sure to delete all issues and labels from https://github.com/${repo}? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo 'You might need to type "reset" to restore your terminal afterward'
 
-gh issue list -R "${REPO}" -L=1000 -s=all  --json=number | jq -r '.[].number' | xargs -n 1 -P 10 gh issue -R "${REPO}" delete
-gh label list -R "${REPO}" --limit=500 --json name | jq -r '.[].name' | xargs -L 1  -P 10 -I {} gh label delete --confirm -R "${REPO}" '{}'
+    # TODO: loop if more than 1000 issues imported
+    gh issue list -R "${repo}" --limit=1000 --state=all --json number --jq '.[].number' | xargs -n 1 -P 10 gh issue -R "${repo}" delete --yes
+    gh label list -R "${repo}" --limit=500 --json name --jq '.[].name' | xargs -L 1 -I {} gh label delete --yes -R "${repo}" '{}'
+fi
