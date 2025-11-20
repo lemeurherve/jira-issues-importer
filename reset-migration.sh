15602faef84f0f03d23efd289d51cd8392aa7727
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2025-11-20 17:10:11 +0100

    gh issue delete --yes to avoid borking terminal

--- reset-migration.sh
+++ reset-migration.sh
@@ -4,6 +4,9 @@ REPO=$1
 
 # caution make sure anything you want to keep is managed in code
 # i.e. existing labels that may be used for pull requests
+# export NO_COLOR=true
+# export GH_FORCE_TTY=never
 
-gh issue list -R "${REPO}" -L=1000 -s=all  --json=number | jq -r '.[].number' | xargs -n 1 -P 10 gh issue -R "${REPO}" delete
-gh label list -R "${REPO}" --limit=500 --json name | jq -r '.[].name' | xargs -L 1  -P 10 -I {} gh label delete --confirm -R "${REPO}" '{}'
+# TODO: loop if more than 1000 issues imported
+gh issue list -R "${REPO}" --limit=1000 --state=all --json number | jq -r '.[].number' | xargs -n 1 -P 10 gh issue -R "${REPO}" delete --yes
+gh label list -R "${REPO}" --limit=500 --json name | jq -r '.[].name' | xargs -L 1 -I {} gh label delete --yes -R "${REPO}" '{}'
