677ca98659b229df88faa884434dab03d00113ba
Tim Jacomb <timjacomb1@gmail.com>
2025-12-08 21:49:11 +0000

    Cleanup

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -f issue_comment_on_JENKINS-* jira-keys-to-github-id* jira-to-github-mapping* combined-remotelinks.txt combined-unique-watchers-emails.txt combined-watchers-usernames-and-emails.txt jira_output/*
+
