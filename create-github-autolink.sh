6d6793d74fede5c132d7c50d46335187403186f4
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2025-12-17 09:15:42 +0100

    fix: allow replacing existing autolink, exit cleanly otherwise (#80)

--- create-github-autolink.sh
+++ create-github-autolink.sh
@@ -7,6 +7,12 @@ set -e -o pipefail
 : "${JIRA_MIGRATION_JIRA_PROJECT_NAME:? Missing Jira project name (e.g., JENKINS)}"
 : "${JIRA_MIGRATION_REDIRECTION_SERVICE? Missing URL of the redirection service (e.g., https://issue-redirect.jenkins.io)}"
 : "${JIRA_MIGRATION_CREATE_GITHUB_AUTOLINK:=false}"
+: "${JIRA_MIGRATION_REPLACE_EXISTING_GITHUB_AUTOLINK:=false}"
+
+if [[ "${JIRA_MIGRATION_CREATE_GITHUB_AUTOLINK}" == "false" ]]; then
+  echo "JIRA_MIGRATION_CREATE_GITHUB_AUTOLINK set to 'false', no GitHub autolink created."
+  exit 0
+fi
 
 OWNER=${1:-timja-org}
 REPO=${2:-jenkins-gh-issues-poc-11-04}
@@ -19,9 +25,25 @@ if ! command -v gh >/dev/null 2>&1; then
 fi
 
 github_repo="${OWNER}/${REPO}"
+key_prefix="${JIRA_MIGRATION_JIRA_PROJECT_NAME}-"
 
-if [[ "${JIRA_MIGRATION_CREATE_GITHUB_AUTOLINK}" == "true" ]]; then
-  gh repo autolink create "${JIRA_MIGRATION_JIRA_PROJECT_NAME}-" "${JIRA_MIGRATION_REDIRECTION_SERVICE}/${JIRA_MIGRATION_JIRA_PROJECT_NAME}-<num>" --numeric --repo "${github_repo}"
-else
-  echo "JIRA_MIGRATION_CREATE_GITHUB_AUTOLINK set to 'false', no GitHub autolink created."
+all_autolinks="$(gh repo autolink list --repo "${github_repo}" --json id,isAlphanumeric,keyPrefix,urlTemplate)"
+all_key_prefixes="$(echo "${all_autolinks}" | jq '.[].keyPrefix')"
+existing_autolink_id=""
+if [[ "${all_key_prefixes}" == *"${key_prefix}"* ]]; then
+  existing_autolink="$(echo "${all_autolinks}" | jq --arg kp "$key_prefix" '.[] | select(.keyPrefix == $kp)')"
+  existing_autolink_id="$(echo "${existing_autolink}" | jq -r '.id')"
+  echo "There is already an autolink in ${github_repo} using '${key_prefix}' prefix:"
+  echo "${existing_autolink}" | jq
 fi
+
+if [[ "${JIRA_MIGRATION_REPLACE_EXISTING_GITHUB_AUTOLINK}" == "false" && -n "${existing_autolink_id}" ]]; then
+  echo "JIRA_MIGRATION_REPLACE_EXISTING_GITHUB_AUTOLINK set to 'false', not replacing the existing autolink"
+  exit 0
+fi
+
+if [[ -n "${existing_autolink_id}" ]]; then
+  gh repo autolink delete "${existing_autolink_id}" --repo "${github_repo}" --yes
+fi
+
+gh repo autolink create "${key_prefix}" "${JIRA_MIGRATION_REDIRECTION_SERVICE}/${key_prefix}<num>" --numeric --repo "${github_repo}"
